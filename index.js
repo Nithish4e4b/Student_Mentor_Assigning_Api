ad42398f5c76731638b04dc3dda73b4c18b22957
Nithish Kumar N <127470114+Nithish4e4b@users.noreply.github.com>
2023-07-03 20:35:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.get("/", (req, res) => {
-    res.status(200).send("Mentor-Student_Assigning API by Nivetha is running successfully!");
+    res.status(200).send("Mentor-Student_Assigning API by Nithish is running successfully!");
   });
 
 
